551a5001235a8ba2a3d9fccd98e8306721184003
CBlumini <72533588+CBlumini@users.noreply.github.com>
2023-05-22 22:34:54 -0700

    working on PIO

--- src/meth_controller_base_switches.cpp
+++ src/meth_controller_base_switches.cpp
@@ -48,22 +48,6 @@ const char index_html[] PROGMEM = R"rawliteral(
 </html>
 )rawliteral";
 
-// Replaces placeholder with button section in your web page
-String processor(const String& var){
-  //Serial.println(var);
-  if(var == "BUTTONPLACEHOLDER"){
-    String buttons ="";
-    for(int i=1; i<=NUM_OUTPUTS; i++){
-      String relayStateValue = relayState(i);
-      buttons+= "<h4>Relay #" + String(i) + " - GPIO " + relayGPIOs[i-1] + "</h4><label class=\"switch\"><input type=\"checkbox\" onchange=\"toggleCheckbox(this)\" id=\"" + String(i) + "\" "+ relayStateValue +"><span class=\"slider\"></span></label>";
-    }
-    
-
-    return buttons;
-  }
-  return String();
-}
-
 String relayState(int numRelay){
   if(NORMALLY_OPEN){
     if(digitalRead(relayGPIOs[numRelay-1])){
@@ -83,6 +67,24 @@ String relayState(int numRelay){
   }
   return "";
 }
+
+
+// Replaces placeholder with button section in your web page
+String processor(const String& var){
+  //Serial.println(var);
+  if(var == "BUTTONPLACEHOLDER"){
+    String buttons ="";
+    for(int i=1; i<=NUM_OUTPUTS; i++){
+      String relayStateValue = relayState(i);
+      buttons+= "<h4>Relay #" + String(i) + " - GPIO " + relayGPIOs[i-1] + "</h4><label class=\"switch\"><input type=\"checkbox\" onchange=\"toggleCheckbox(this)\" id=\"" + String(i) + "\" "+ relayStateValue +"><span class=\"slider\"></span></label>";
+    }
+    
+
+    return buttons;
+  }
+  return String();
+}
+
 
 void setup(){
   Serial.begin(115200);
