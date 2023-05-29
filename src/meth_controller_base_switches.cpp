c65916f21eb584ac5864d6a1d5f64e394d65728d
CBlumini <72533588+CBlumini@users.noreply.github.com>
2023-05-29 16:48:22 -0700

    Reformatted HTML, working

--- src/meth_controller_base_switches.cpp
+++ src/meth_controller_base_switches.cpp
@@ -39,66 +39,81 @@ const char* STATE_REF = "state";
 
 
 const char index_html[] PROGMEM = R"rawliteral(
-<!DOCTYPE HTML><html>
+<!DOCTYPE HTML>
+<html>
 <head>
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <style>
-    html {font-family: Arial; display: inline-block; text-align: center;}
-    h2 {font-size: 3.0rem;}
-    p {font-size: 3.0rem;}
-    body {max-width: 600px; margin:0px auto; padding-bottom: 25px;}
+    html {
+      font-family: Arial;
+      display: inline-block;
+      text-align: center;
+    }
+    h2 {
+      font-size: 3.0rem;
+    }
+    p {
+      font-size: 3.0rem;
+    }
+    body {
+      max-width: 600px;
+      margin: 0px auto;
+      padding-bottom: 25px;
+    }
     .switch {
-      position: relative; 
-      display: inline-block; 
-      width: 120px; 
-      height: 68px
-      } 
+      position: relative;
+      display: inline-block;
+      width: 120px;
+      height: 68px;
+    } 
     .switch input {
-      display: none
-      }
+      display: none;
+    }
     .slider {
-      position: absolute; 
-      top: 0; 
-      left: 0; 
-      right: 0; 
-      bottom: 0; b
-      ackground-color: #ccc; 
-      border-radius: 34px}
+      position: absolute;
+      top: 0;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      background-color: #ccc;
+      border-radius: 34px;
+    }
     .slider:before {
-      position: absolute; 
-      content: ""; 
-      height: 52px; 
-      width: 52px; 
-      left: 8px; 
-      bottom: 8px; 
-      background-color: #fff; 
-      -webkit-transition: .4s; 
-      transition: .4s; 
-      border-radius: 68px
-      }
-    input:checked+.slider {
-      background-color: #2196F3
-      }
-    input:checked+.slider:before {
-      -webkit-transform: translateX(52px); 
-      -ms-transform: translateX(52px); 
-      transform: translateX(52px)}
+      position: absolute;
+      content: "";
+      height: 52px;
+      width: 52px;
+      left: 8px;
+      bottom: 8px;
+      background-color: #fff;
+      -webkit-transition: .4s;
+      transition: .4s;
+      border-radius: 68px;
+    }
+    input:checked + .slider {
+      background-color: #2196F3;
+    }
+    input:checked + .slider:before {
+      -webkit-transform: translateX(52px);
+      -ms-transform: translateX(52px);
+      transform: translateX(52px);
+    }
   </style>
 </head>
 <body>
   <h3>Blumini Injects Monitor and Test Page</h3>
   %BUTTONPLACEHOLDER%
-<script>
-  function toggleCheckbox(element) {
-    var xhr = new XMLHttpRequest();
-    if(element.checked){
-      xhr.open("GET", "/update?relay=" + element.id + "&state=1", true);
-    } else {
-      xhr.open("GET", "/update?relay=" + element.id + "&state=0", true);
+  <script>
+    function toggleCheckbox(element) {
+      var xhr = new XMLHttpRequest();
+      if (element.checked) {
+        xhr.open("GET", "/update?relay=" + element.id + "&state=1", true);
+      } else {
+        xhr.open("GET", "/update?relay=" + element.id + "&state=0", true);
+      }
+      xhr.send();
     }
-    xhr.send();
-  }
-</script>
+  </script>
 </body>
 </html>
 )rawliteral";
@@ -153,10 +168,6 @@ String processor(const String& var){
   //Serial.println(var);
   if(var == "BUTTONPLACEHOLDER"){
     String buttons ="";
-    // for(int i=1; i<=NUM_OUTPUTS; i++){
-    //   String relayStateValue = getOutputStatus(i);
-    //   buttons+= "<h4>Relay #" + String(i) + " - GPIO " + actuatorOutputs[i-1] + "</h4><label class=\"switch\"><input type=\"checkbox\" onchange=\"toggleCheckbox(this)\" id=\"" + String(i) + "\" "+ relayStateValue +"><span class=\"slider\"></span></label>";
-    // }
     String pumpState = getOutputStatus(0);
     buttons = "<h4>Pump Test</h4><label class=\"switch\"><input type=\"checkbox\" onchange=\"toggleCheckbox(this)\" id = 0 "+ pumpState +"><span class=\"slider\"></span></label>";
     String valveState = getOutputStatus(1);
@@ -234,7 +245,6 @@ void setup(){
     }
     else {
       relayId = "No message sent";
-      // idParam = "none";
     }
     Serial.println(relayId + relayState);
     request->send(200, "text/plain", "OK");
