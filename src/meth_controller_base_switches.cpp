0ffd4b232fdf116330bbfb17625ef5b117e52dee
CBlumini <72533588+CBlumini@users.noreply.github.com>
2023-05-30 22:56:25 -0700

    Added seperate endpoints fro the two text boxes

--- src/meth_controller_base_switches.cpp
+++ src/meth_controller_base_switches.cpp
@@ -8,22 +8,19 @@
 // as you just need to refresh the browser as opposed to reconnection to the web server
 #define USE_INTRANET
 
-// replace this with your homes intranet connect parameters
-// moved this to a special file
-
 // once  you are read to go live these settings are what you client will connect to
 #define AP_SSID "Water Inject"
 #define AP_PASS NULL
 
-#define NORMALLY_OPEN  true
+#define NORMALLY_OPEN true
 
-// Netwroking Setup
+// Networking Setup
 IPAddress PageIP(192, 168, 1, 1);
 IPAddress gateway(192, 168, 1, 1);
 IPAddress subnet(255, 255, 255, 0);
 IPAddress ip;
 // variable for the IP reported when you connect to your homes intranet (during debug mode)
-IPAddress Actual_IP;
+IPAddress actualIP;
 
 AsyncWebServer server(80);
 
@@ -31,12 +28,14 @@ int actuatorOutputs[] = {2, 26};
 // int NUM_OUTPUTS = sizeof(relayGPIOs)/sizeof(relayGPIOs[0]);
 int NUM_OUTPUTS = 2;
 
-const char* RELAY_REF = "relay";  
-const char* STATE_REF = "state";
+const char *RELAY_REF = "relay";
+const char *STATE_REF = "state";
 int injectionStartRPM = 0;
 int injectionEndRPM = 0;
 float scalingFactor = 0.0;
 
+// html code
+#pragma region
 const char index_html[] PROGMEM = R"rawliteral(
 <!DOCTYPE HTML>
 <html>
@@ -128,28 +127,50 @@ const char index_html[] PROGMEM = R"rawliteral(
     }
     function updateStartRPM() {
       let startRPM = document.getElementById('var1').value;
+      // Validation
       if (!Number.isInteger(Number(startRPM))){
         alert("Must be a whole number");
         return;
       }
-      var xhr = new XMLHttpRequest();
+      let xhr = new XMLHttpRequest();
       xhr.open("GET", "/updateStartRPM?startRPM=" + startRPM);
+
+      // let the user know it worked
+      xhr.onload = () => {
+        if (xhr.status == 200) {
+            alert("Updated Start RPM to", xhr.responseText)
+        } else {
+            alert("Failed to update Start RPM, Error: ", xhr.status)
+        }
+      };
       
-      xhr.onerror = function() {
+      // Error if the request deosnt go through
+      xhr.onerror = () => {
         alert("Request failed.");
       };
       xhr.send();
     }
     function updateEndRPM() {
-      let endRPM = document.getElementById('var1').value;
+      let endRPM = document.getElementById('var2').value;
+      // Validation
       if (!Number.isInteger(Number(endRPM))){
         alert("Must be a whole number");
         return;
       }
-      var xhr = new XMLHttpRequest();
-      xhr.open("GET", "/updateEndtRPM?endRPM=" + endRPM);
+      let xhr = new XMLHttpRequest();
+      xhr.open("GET", "/updateEndRPM?endRPM=" + endRPM);
+
+      // let the user know it went througg
+      xhr.onload = () => {
+        if (xhr.status == 200) {
+            alert("Updated End RPM")
+        } else {
+            alert("Failed to update End RPM, Error: ", xhr.status)
+        }
+      };
       
-      xhr.onerror = function() {
+      // Error if the request doesnt go through
+      xhr.onerror = () => {
         alert("Request failed.");
       };
       xhr.send();
@@ -159,8 +180,11 @@ const char index_html[] PROGMEM = R"rawliteral(
 </html>
 )rawliteral";
 
+#pragma endregion
+
 // I think I got this code from the wifi example
-void printWifiStatus() {
+void printWifiStatus()
+{
 
   // print the SSID of the network you're attached to:
   Serial.print("SSID: ");
@@ -183,37 +207,44 @@ void printWifiStatus() {
 
 // end of code
 
-String getOutputStatus(int numRelay){
-  if(NORMALLY_OPEN){
-    if(digitalRead(actuatorOutputs[numRelay-1])){
+String getOutputStatus(int numRelay)
+{
+  if (NORMALLY_OPEN)
+  {
+    if (digitalRead(actuatorOutputs[numRelay - 1]))
+    {
       return "";
     }
-    else {
+    else
+    {
       return "checked";
     }
   }
-  else {
-    if(digitalRead(actuatorOutputs[numRelay-1])){
+  else
+  {
+    if (digitalRead(actuatorOutputs[numRelay - 1]))
+    {
       return "checked";
     }
-    else {
+    else
+    {
       return "";
     }
   }
   return "";
 }
 
-
 // Replaces placeholder with button section in your web page
-String processor(const String& var){
-  //Serial.println(var);
-  if(var == "BUTTONPLACEHOLDER"){
-    String buttons ="";
+String processor(const String &var)
+{
+  // Serial.println(var);
+  if (var == "BUTTONPLACEHOLDER")
+  {
+    String buttons = "";
     String pumpState = getOutputStatus(0);
-    buttons = "<h4>Pump Test</h4><label class=\"switch\"><input type=\"checkbox\" onchange=\"toggleCheckbox(this)\" id = 0 "+ pumpState +"><span class=\"slider\"></span></label>";
+    buttons = "<h4>Pump Test</h4><label class=\"switch\"><input type=\"checkbox\" onchange=\"toggleCheckbox(this)\" id = 0 " + pumpState + "><span class=\"slider\"></span></label>";
     String valveState = getOutputStatus(1);
-    buttons += "<h4>Valve Test</h4><label class=\"switch\"><input type=\"checkbox\" onchange=\"toggleCheckbox(this)\" id = 1 "+ valveState +"><span class=\"slider\"></span></label>";
-    
+    buttons += "<h4>Valve Test</h4><label class=\"switch\"><input type=\"checkbox\" onchange=\"toggleCheckbox(this)\" id = 1 " + valveState + "><span class=\"slider\"></span></label>";
 
     return buttons;
   }
@@ -221,29 +252,34 @@ String processor(const String& var){
 }
 
 ///////////////// MAIN CODE ///////////////////
-void setup(){
+void setup()
+{
   Serial.begin(115200);
 
   // Set up the pinst
-  for(int i=1; i<=NUM_OUTPUTS; i++){
-    pinMode(actuatorOutputs[i-1], OUTPUT);
-    if(NORMALLY_OPEN){
-      digitalWrite(actuatorOutputs[i-1], HIGH);
+  for (int i = 1; i <= NUM_OUTPUTS; i++)
+  {
+    pinMode(actuatorOutputs[i - 1], OUTPUT);
+    if (NORMALLY_OPEN)
+    {
+      digitalWrite(actuatorOutputs[i - 1], HIGH);
     }
-    else{
-      digitalWrite(actuatorOutputs[i-1], LOW);
+    else
+    {
+      digitalWrite(actuatorOutputs[i - 1], LOW);
     }
   }
-  
 
 #ifdef USE_INTRANET
   WiFi.begin(LOCAL_SSID, LOCAL_PASS);
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(500);
     Serial.print(".");
   }
-  Serial.print("IP address: "); Serial.println(WiFi.localIP());
-  Actual_IP = WiFi.localIP();
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+  actualIP = WiFi.localIP();
 #endif
 
   // if you don't have #define USE_INTRANET, use AP Mode
@@ -253,19 +289,19 @@ void setup(){
   WiFi.softAPConfig(PageIP, gateway, subnet);
   delay(100);
   Actual_IP = WiFi.softAPIP();
-  Serial.print("IP address: "); Serial.println(Actual_IP);
+  Serial.print("IP address: ");
+  Serial.println(Actual_IP);
 #endif
-////////////// END WIFI SETUP//////////
+  ////////////// END WIFI SETUP//////////
 
-
-///////////////SETUO ROUTES////////////
-// Route for root / web page
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send_P(200, "text/html", index_html, processor);
-  });
+  ///////////////SETUO ROUTES////////////
+  // Route for root / web page
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
+            { request->send_P(200, "text/html", index_html, processor); });
 
   // Send a GET request to <ESP_IP>/update?relay=<inputMessage>&state=<inputMessage2>
-  server.on("/update", HTTP_GET, [] (AsyncWebServerRequest *request) {
+  server.on("/update", HTTP_GET, [](AsyncWebServerRequest *request)
+            {
     String relayId;
     String idParam;
     String relayState;
@@ -289,10 +325,10 @@ void setup(){
       relayId = "No message sent";
     }
     Serial.println(relayId + relayState);
-    request->send(200, "text/plain", "OK");
-  });
+    request->send(200, "text/plain", "OK"); });
 
-  server.on("/updateVariables", HTTP_GET, [] (AsyncWebServerRequest *request) {
+  server.on("/updateVariables", HTTP_GET, [](AsyncWebServerRequest *request)
+            {
   if (request->hasParam("var1") && request->hasParam("var2")) {
     injectionStartRPM = request->getParam("var1")->value().toInt();
     injectionEndRPM = request->getParam("var2")->value().toInt();
@@ -301,13 +337,32 @@ void setup(){
     Serial.println("Injection start RPM is: " + String(injectionEndRPM));
   }
 
-  request->send(200, "text/plain", "OK");
-});
+  request->send(200, "text/plain", "OK"); });
+
+  server.on("/updateStartRPM", HTTP_GET, [](AsyncWebServerRequest *request)
+            {
+  if (request->hasParam("startRPM")) {
+    injectionStartRPM = request->getParam("startRPM")->value().toInt();
+    Serial.println("Injection Start RPM is: " + String(injectionStartRPM));
+  }
+
+  request->send(200, "text/plain", String(injectionStartRPM)); 
+  });
+
+  server.on("/updateEndRPM", HTTP_GET, [](AsyncWebServerRequest *request)
+            {
+  if (request->hasParam("endRPM")) {
+    injectionEndRPM = request->getParam("endRPM")->value().toInt();
+    Serial.println("Injection End RPM is: " + String(injectionEndRPM));
+  }
+
+  request->send(200, "text/plain", String(injectionEndRPM)); 
+  });
 
   // Start server
   server.begin();
 }
 
-void loop() {
-
+void loop()
+{
 }
